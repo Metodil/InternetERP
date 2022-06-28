618c982b9e4a4dcd1966932f96a409b6b9f109c4
Metodi Lichkov <metodil@hotmail.com>
2022-06-28 11:29:01 +0300

    Program test

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication
         {
             var db = new InternetERPDbContext();
             db.Database.Migrate();
+            // test
             db.SaveChanges();
 
         }
