deceb12a7f409a1510d9993040c0bd4c997f5375
Metodi Lichkov <metodil@hotmail.com>
2022-06-28 11:23:16 +0300

    Mod Console UI 1

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -11,41 +11,6 @@ namespace ConsoleApplication
         {
             var db = new InternetERPDbContext();
             db.Database.Migrate();
-            //db.Database.EnsureDeleted();
-            //db.Database.EnsureCreated();
-
-            //var employee = new Employee
-            //{
-            //    FullName = "Методи Личков",
-            //    HireDate = DateTime.UtcNow,
-            //    Salary = 1500,
-            //    JobbTitle = new JobTitle
-            //    {
-            //        Name = "Мениджър"0
-            //    },
-            //    Role =  new Role
-            //    {
-            //        Name = "Администратор"
-            //    }
-
-            //};
-            //var town = new Town
-            //{
-            //    Name = "Пазарджик"
-            //};
-            //var address = new Address
-            //{
-            //    District = "Чиксалам",
-            //    Street = "Георги Бенковски 60",
-            //    Note = "Учи EF Core",
-            //    Town = new Town
-            //    {
-            //        Name = "Пазарджик"
-            //    },
-
-            //};
-            //employee.Address = address;
-            //db.Employees.Add(employee);
             db.SaveChanges();
 
         }
